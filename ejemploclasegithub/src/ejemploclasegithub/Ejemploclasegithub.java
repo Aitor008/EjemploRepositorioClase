fd01fa21a9a8f55dc6d95388af4d684d1d5d8089
z1ku <z1ku1337@gmail.com>
2022-03-24 13:58:20 +0100

    Mayonesa

--- ejemploclasegithub/src/ejemploclasegithub/Ejemploclasegithub.java
+++ ejemploclasegithub/src/ejemploclasegithub/Ejemploclasegithub.java
@@ -5,6 +5,7 @@ public class Ejemploclasegithub {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Adios");
+        System.out.println("Mayonesa");
     }
     
 }
